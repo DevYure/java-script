aab23688e4017436cd8bf11b5b341d1a47f1e4bf
DevYure <122752006+DevYure@users.noreply.github.com>
2023-01-29 07:58:14 -0300

    atualziar aula 13

---
+++ Aula 13/repetição.js
@@ -0,0 +1,13 @@
+ /* repetição laço*/
+
+ var n1 = 1
+
+ /*while (n1 <=30){
+    console.log(`Prostituta ${n1}`)
+    n1++/* soma 1 toda vez
+ }*/
+
+ do {
+    console.log(`Prostituta ${n1}`)
+    n1++
+ }while (n1 <= 50)
