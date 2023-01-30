f3ae398f2c228b22a7e00b9bd73ea3702fe83bb2
DevYure <122752006+DevYure@users.noreply.github.com>
2023-01-30 09:24:04 -0300

    atualizar

--- Aula Exercicios/EX003/java.js
+++ Aula Exercicios/EX003/java.js
@@ -4,17 +4,24 @@ function contar(){
     var passos = document.getElementById('Passos')
     var texto = document.getElementById('textoresultado')
     var resultado = document.getElementById('resultado')
-    var inicio2 = Number(inicio.value)
-    var fim2 = Number(fim.value)
-    var passos2 = Number (passos.value)
-    if (inicio == 0 || fim == 0 || passos == 0){
+    if (inicio.value == 0 || fim.value == 0 || passos.value == 0){
         window.alert("Sem dados cadastrado, por vafor prenchar novamente")
     }
     else{
         texto.innerHTML = "Contando :"
-        for( y = inicio2; y <=fim2; y += passos2){
-            resultado.innerHTML += `${y} `
+        var inicio2 = Number(inicio.value)
+        var fim2 = Number(fim.value)
+        var passos2 = Number (passos.value)
+        if (inicio2 < fim2){
+            for( y = inicio2; y <=fim2; y += passos2){
+                resultado.innerHTML += `${y} \u{1F449}`
+            }
         }
+        else {
+            for (y = inicio2; y >= fim2; y-= passos2)
+            resultado.innerHTML += `${y} \u{1F449}`
+        }
+        resultado.innerHTML += `\u{1F3C1}`
     }
     
 }
