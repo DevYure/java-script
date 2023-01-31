37023ce76092519dba2150674456f62d902a2518
DevYure <122752006+DevYure@users.noreply.github.com>
2023-01-31 07:12:32 -0300

    arualizar

--- Aula Exercicios/EX006/java.js
+++ Aula Exercicios/EX006/java.js
@@ -1,6 +1,7 @@
 function fazertabuada(){
     var número = document.getElementById('número')
     var resposta = document.getElementById('resposta')
+    var img1 = document.getElementById('parafoto')
     if (número.value == 0){
         window.alert("Erro,Não possui valor, Porfavor Digite novamente Um número a baixo")
     }
@@ -14,6 +15,9 @@ function fazertabuada(){
             c++
             item.value = `resposta${c}`
             resposta.appendChild(item)
+            let img = document.createElement("img")
+            img.src = 'imagens/foto.jpg'
+            img1.appendChild(img)
         }
     }
 }
