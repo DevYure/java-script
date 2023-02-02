4e32f785048f6e20ae42aa0b59c8ede63563a4a4
DevYure <122752006+DevYure@users.noreply.github.com>
2023-02-02 10:37:19 -0300

    atualização

--- Aula Exercicios/EX007/java.js
+++ Aula Exercicios/EX007/java.js
@@ -1,35 +1,31 @@
-let tab = document.getElementById('tabela')
-let res = document.getElementById('res')
-let sequencia = []
-function adicionar(){
-var num = document.getElementById('num')
-if( num.value == 0){
-    window.alert("[ERRO], Não Possui Numero para seram Adicionados")
-}
-else if (num.value > 100 || num.value < 1){
-    window.alert("[ERRO] Número fora dos parametros")
-}
-else if (num.value < 100 && num.value > 1 ){
-    let item = document.createElement('option')
-    let numero = Number(num.value)
-    tab.appendChild(item)
-    item.text = `O número ${numero} foi adicionado`
-}
-}
+var num = document.getElementById('num');
+var tab = document.getElementById('tabela');
+let sequencia = [];
+var res = document.getElementById('res');
 
-function verificar() {
-    if (sequencia != 0 ){
-        sequencia.push(Number(num.value))
-        sequencia.sort()
-        let total = sequencia.lenght
-        let resposta = document.createElement('p')
-        let resposta2 = document.createElement('p')
-        let resposta3 = document.createElement('p')
-        let resposta4 = document.createElement('p')
-        let resposta5 = document.createElement('p')
-        resposta.text = `O total de números dadastrado foi ${total}`
+function adicionar(){
+    let numero = Number(num.value)
+    if(sequencia.indexOf(numero) !== -1){
+        window.alert('Número já Escolido')
+    } else if (numero.lenght == 0){
+        window.alert('Digite um numero')
+    } else if (numero < 1 || numero > 100){
+        window.alert('Fora dos Parametros')
+    }
+    else{
+        sequencia.push(numero)
+        let item = document.createElement('option')
+        item.text= `O Número ${numero} foi adidionado com sucesso`
+        tab.appendChild(item)
     }
 }
 
+function verificar(){
+    let numero = Number(num.value)
+    let item2 = document.createElement('p')
+    let ordem = sequencia.length
+    res.appendChild(item2)
+    item.innerHTML = `Na contagem tem ${ordem} temos na sequencia`
 
-
+    
+}
