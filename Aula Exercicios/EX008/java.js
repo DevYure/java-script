0696b9515c886a95de43650456552605bc0c3275
DevYure <122752006+DevYure@users.noreply.github.com>
2023-02-03 08:03:58 -0300

    atualizar

--- Aula Exercicios/EX008/java.js
+++ Aula Exercicios/EX008/java.js
@@ -4,12 +4,30 @@ var res = document.getElementById('res')
 var numeros = []
 
 function adicionar() {
-    var numero = Number(num.value)
-    if(numeros.indexOf(numero)!== -1) {
-        window.alert('Valor ja Celecionado, Por Favor escolha outro número')
-    }else if(numero.lenght == 0){
-        window.alert('[ERRO] Valor não encontrado, Por Favor Digite um Número')
-    }else if (numero < 1 || numero > 100){
-        window.alert('Valor fora da Área Aceitavel, Por Farvor Escreva um número entre 1 e 100')
+    let n = Number(num.value)
+    if(numeros.indexOf(n) !== -1){
+        window.alert('Número ja na tabela')
+    }else if (numeros.length == 0){
+        window.alert('Digite Um Número')
+    }else if (n <= 1 && n >= 100){
+        window.alert('Froa dos Parametros')
+    }else{
+        let item = document.createElement('option')
+        item.text = `O Número ${n} Foi Adicionado`
+        tab.appendChild(item)
+        numeros.push(n)
+       
+    }
+}
+
+function verificar(){
+    if(numeros.length == 0){
+        window.alert('[ERRO] Digite um número')
+    }else{
+        numeros.push(n)
+        let total = document.createComment('option')
+        let tot = numeros.length
+        total.text = `O Conjunto Possui ${tot} Números`
+        tab.innerHTML = ""
     }
 }
