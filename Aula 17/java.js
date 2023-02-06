94d54d016540923d8baaba6e52ffc0622df45629
DevYure <122752006+DevYure@users.noreply.github.com>
2023-02-06 09:14:43 -0300

    admin

---
+++ Aula 17/java.js
@@ -0,0 +1,16 @@
+class Produto {
+    Adicionar(){
+        
+        this.id
+        this.name
+        this.valor
+        window.alert('Item Adicionado a Lista de Compras')
+    }
+
+    Remover(){
+        window.alert("Item retirado da Lista de Compras")
+    }
+}
+
+var produto = new Produto
+
